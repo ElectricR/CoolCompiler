cc6c3d9527a31d12ceb24242bd6d96c6e58d23ac
ElectricR <chulkovroma@mail.ru>
2021-10-13 17:48:40 +0300

    Stderr test capture

--- tests/parser_tests/parser_end_to_end.py
+++ tests/parser_tests/parser_end_to_end.py
@@ -7,10 +7,10 @@ class LexerTest(unittest.TestCase):
         print(f"Checking {file_name}...")
         reference_lexer_proc = subprocess.Popen(["./reference/bin/lexer", *file_name], stdout=subprocess.PIPE)
         reference_lexer_out = reference_lexer_proc.communicate()[0]
-        reference_parser_proc = subprocess.Popen(["./reference/bin/parser"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        reference_parser_proc = subprocess.Popen(["./reference/bin/parser"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         reference_out = reference_parser_proc.communicate(reference_lexer_out)[0]
 
-        my_proc = subprocess.Popen(["./parser", *file_name], stdout=subprocess.PIPE)
+        my_proc = subprocess.Popen(["./parser", *file_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         my_out = my_proc.communicate()[0]
         if reference_parser_proc.returncode == 0:
             self.assertEqual(reference_out.decode("UTF-8"), my_out.decode("UTF-8"))
